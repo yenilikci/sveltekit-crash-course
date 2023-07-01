a2bfc80411f9e29e6519202434eb60eb5151cbe7
yenilikci <m.melihcelik017@outlook.com>
2023-07-01 16:37:03 +0300

    lesson 8

---
+++ 8-loading-data-ssr/src/routes/guides/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let data;
+  $: guides = data.guides;
+</script>
+
+<div class="guides">
+  <ul>
+    {#each guides as guide}
+      <li>
+        <a href="/">{guide.title}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .guides {
+    margin-top: 20px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  a {
+    display: inline-block;
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px dotted rgba(255, 255, 255, 0.2);
+  }
+</style>
